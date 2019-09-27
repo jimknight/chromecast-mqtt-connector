ced6fd3d2f6acea13af1058f28dd97c81ac01c60
Jim Knight <jim@lavatech.com>
2019-09-27 08:51:32 -0400

    Updated the kill to avoid running with error if not started

--- kill.sh
+++ kill.sh
@@ -1,1 +1,8 @@
-kill $(ps -efw | grep connector.py | grep -v grep | awk '{print $2}')
+myvar=$(ps -efw | grep connector.py | grep -v grep | awk '{print $2}')
+if test -z "$myvar"
+then
+  echo "empty"
+else
+  echo "not empty"
+  kill $(ps -efw | grep connector.py | grep -v grep | awk '{print $2}')
+fi
---
+++ status.sh
@@ -0,0 +1,1 @@
+ps -efw | grep connector.py
